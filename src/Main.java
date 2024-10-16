b4cd34ff8f78f68100dc69de1c5acc67b10d9823
Admin <galinapaskina61@gmail.com>
2024-10-16 20:26:41 +0400

    work 4

---
+++ src/Main.java
@@ -0,0 +1,62 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задание 1");
+        int clientOS = 0;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке.");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке.");
+        } else {
+            System.out.println("Нет такой операционной системы.");
+        }
+        System.out.println("Задание 2");
+        int clientOs = 0;
+        int clientDeviceYear = 2015;
+        if (clientOs == 0 && clientDeviceYear <= 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+        } else if (clientOs != 0 && clientDeviceYear <= 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке.");
+        } else {
+            System.out.println("Установите приложение.");
+        }
+
+        System.out.println("Задание 3");
+        int year = 2020;
+        boolean leapBone = true;
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    leapBone = true;
+                } else {
+                    leapBone = true;
+                }
+            }
+            if (leapBone) {
+                System.out.println(year + " год високосный");
+            } else {
+                System.out.println(year + " год не високосный");
+            }
+        }
+        System.out.println("Задание 4");
+        int deliveryDistance = 95;
+        int travelTime = 1;
+        if (deliveryDistance < 20) {
+            travelTime = 1;
+        } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
+            travelTime = 2;
+
+        } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            travelTime = 3;
+        } else {
+            travelTime = -1;
+        } if (travelTime != -1) {
+            System.out.println("Потребуется дней: " + travelTime + " дня срок доставки.");
+        } else { System.out.println("Доставки нет");
+        }
+
+
+
+
+    }
+
+}
